a20ea2facf54c09bb53cf1da1c4444debc561eee
ajayvyas325 <82799310+ajayvyas325@users.noreply.github.com>
2024-06-29 12:12:26 +0000

    Push other data

--- 01_basics/04_Comparison.js
+++ 01_basics/04_Comparison.js
@@ -7,12 +7,34 @@
 
 //==
 
-const id = Symbol('123');
-const anotheID = Symbol('123')
+// const id = Symbol('123');
+// const anotheID = Symbol('123')
 
-console.log(id);
-console.log(anotheID);
-console.log(id == anotheID );
+// console.log(id);
+// console.log(anotheID);
+// console.log(id == anotheID );
+
+// let name1 = "Ajay";
+// let name2 = name1;
+// console.log(name2);
+// name2 = "Ajax";
+// console.log(name2);
+// console.log(name1);
+
+let Var1 = {
+    nameOne : "Ajay",
+    upi : "Patym@pyt"
+}
+
+
+let nameTwo = Var1;
+nameTwo.nameOne="Ajay Hi"
+console.log(Var1.nameOne);
+console.log(nameTwo.nameOne);
+
+
+
+
 
 
 
---
+++ 02_basics/01_array.js
@@ -0,0 +1,28 @@
+const array = [0,1,2,3,4,5];
+const MyArray= ["Spiderman", "Batman"]
+
+const myArray2 = new Array (0,1,2,3,4,5);
+// console.log(array[0]);
+// array.push(6)
+// array.push(7)
+// array.pop()
+// console.log(array);
+
+// array.unshift(9)
+// array.shift()
+// console.log(array.includes(9));
+// console.log(array.indexOf(800));
+
+// const newArr = array.join()
+
+// console.log(array);
+// console.log(typeof newArr)
+
+console.log("A " + array);
+const myn1 = array.slice(1, 3)
+console.log(myn1);
+
+console.log("B " + array);
+const myn2 = array.splice(1, 4)
+console.log(myn2);
+console.log("c " + array);
